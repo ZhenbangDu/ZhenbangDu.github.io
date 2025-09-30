d10ba1a6f914469de132433f6936ef6f1c2a9573
ZhenbangDu <50868745+ZhenbangDu@users.noreply.github.com>
2025-09-30 22:05:35 +0000

    Deploying to gh-pages from @ ZhenbangDu/ZhenbangDu.github.io@024ef772dd9bd1ec9139ef5b6debd8b9b4d5c09b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -398,11 +398,8 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
-          description: "",
-          section: "News",},{id: "news-i-ll-be-at-iccv-25-in-hawaii-oct-19-23th-in-person-happy-to-chat",
-          title: 'I’ll be at ICCV 25 in Hawaii🌴 (Oct 19 – 23th) in person—happy...',
+            },},{id: "news-i-ll-be-at-iccv-25-in-hawaii-oct-19-23th-in-person-happy-to-chat",
+          title: 'I’ll be at ICCV 25 in Hawaii (Oct 19 – 23th) in person—happy...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
